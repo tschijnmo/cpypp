922fe96b0575fad06615f6bfce6cfda652455bfa
Tschijnmo TSCHAU <tschijnmotschau@gmail.com>
2017-11-30 14:05:32 -0600

    Add tests for borrowing handles
    The borrowing handles are relatively easier to test since they simply
    should not touch reference counts.

--- test/handle.cpp
+++ test/handle.cpp
@@ -167,6 +167,7 @@ TEST_CASE("Handles correctly manages reference counts", "[Handle]")
 
                     handle = handle2;
                     REQUIRE(handle.get() == handle2.get());
+                    REQUIRE(!handle.if_borrow());
                     REQUIRE(Py_REFCNT(two) == init_count2 + 1);
                 }
 
@@ -181,6 +182,8 @@ TEST_CASE("Handles correctly manages reference counts", "[Handle]")
                     REQUIRE(Py_REFCNT(two) == init_count2);
 
                     handle = std::move(handle2);
+                    REQUIRE(handle.get() == two);
+                    REQUIRE(!handle.if_borrow());
                     REQUIRE(Py_REFCNT(two) == init_count2);
                     REQUIRE(!handle2);
                 }
@@ -225,6 +228,202 @@ TEST_CASE("Handles correctly manages reference counts", "[Handle]")
             Py_DECREF(two);
         }
 
+        Py_DECREF(one);
+    }
+
+    SECTION("Borrowing handles")
+    {
+        // Testing of borrowing handles are relatively easy, no matter what
+        // happens, the reference count should never be touched.
+        PyObject* one = Py_BuildValue("i", 1);
+        PyObject* two = Py_BuildValue("i", 2);
+        Py_ssize_t init_count1 = Py_REFCNT(one);
+        Py_ssize_t init_count2 = Py_REFCNT(two);
+        auto check_ref = [&]() {
+            REQUIRE(Py_REFCNT(one) == init_count1);
+            REQUIRE(Py_REFCNT(two) == init_count2);
+        };
+
+        SECTION("touch no reference count from initialization.")
+        {
+            {
+                Handle handle(one, true);
+
+                REQUIRE(handle.get() == one);
+                REQUIRE(handle.if_borrow());
+                REQUIRE(handle);
+                check_ref();
+            }
+            check_ref();
+        }
+
+        SECTION("can be copy initialized")
+        {
+            {
+                Handle handle(one, true);
+
+                Handle handle2(handle);
+                REQUIRE(handle2.get() == one);
+                REQUIRE(handle2.if_borrow());
+                check_ref();
+
+                // The copied handle should not be mutated.
+                REQUIRE(handle.get() == one);
+                REQUIRE(handle.if_borrow());
+            }
+            check_ref();
+        }
+
+        SECTION("can be move initialized")
+        {
+            {
+                Handle handle(one, true);
+
+                Handle handle2(std::move(handle));
+                REQUIRE(handle2.get() == one);
+                REQUIRE(handle2.if_borrow());
+                check_ref();
+
+                REQUIRE(!handle);
+            }
+            check_ref();
+        }
+
+        SECTION("throw exception at null object when asked")
+        {
+            REQUIRE_THROWS_AS(Handle(nullptr, true), Exc_set);
+        }
+
+        SECTION("throw no exception at null object when disabled")
+        {
+            Handle handle(nullptr, true, true);
+            REQUIRE(!handle);
+        }
+
+        SECTION("can release ownership")
+        {
+            {
+                Handle handle(one, true);
+                check_ref();
+                REQUIRE(handle.release() == one);
+                check_ref();
+            }
+            check_ref();
+        }
+
+        SECTION("can be copy assigned with existing managed object")
+        {
+            {
+                Handle handle(one, true);
+                Handle handle2(two, true);
+                check_ref();
+
+                handle = handle2;
+                REQUIRE(handle.get() == handle2.get());
+                REQUIRE(handle.if_borrow());
+                check_ref();
+            }
+
+            check_ref();
+        }
+
+        SECTION("can be move assigned with existing managed object")
+        {
+            {
+                Handle handle(one, true);
+                Handle handle2(two, true);
+                check_ref();
+
+                handle = std::move(handle2);
+                REQUIRE(handle.get() == two);
+                REQUIRE(handle.if_borrow());
+                REQUIRE(!handle2);
+                check_ref();
+            }
+
+            check_ref();
+        }
+
+        SECTION("can be reset with managed object")
+        {
+            {
+                Handle handle{ one, true };
+                handle.reset(two, true);
+
+                REQUIRE(handle.if_borrow());
+                REQUIRE(handle.get() == two);
+                check_ref();
+            }
+
+            check_ref();
+        }
+
+        SECTION("can be copy assigned with no managed object")
+        {
+            {
+                Handle handle{};
+                Handle handle2(two, true);
+                REQUIRE(!handle);
+                check_ref();
+
+                handle = handle2;
+                REQUIRE(handle.get() == handle2.get());
+                REQUIRE(handle.if_borrow());
+                check_ref();
+            }
+
+            check_ref();
+        }
+
+        SECTION("can be move assigned with no management object")
+        {
+            {
+                Handle handle{};
+                Handle handle2(two, true);
+                check_ref();
+
+                handle = std::move(handle2);
+                REQUIRE(handle.get() == two);
+                REQUIRE(handle.if_borrow());
+                REQUIRE(!handle2);
+            }
+
+            check_ref();
+        }
+
+        SECTION("can be reset with no managed object")
+        {
+            {
+                Handle handle{};
+                REQUIRE(!handle);
+
+                handle.reset(two, true);
+                REQUIRE(handle.get() == two);
+                REQUIRE(handle.if_borrow());
+                check_ref();
+            }
+
+            check_ref();
+        }
+
+        SECTION("can be swapped with another handle")
+        {
+            {
+                Handle handle{ one, true };
+                Handle handle2{ two, true };
+
+                handle.swap(handle2);
+                REQUIRE(handle.get() == two);
+                REQUIRE(handle.if_borrow());
+                REQUIRE(handle2.get() == one);
+                REQUIRE(handle2.if_borrow());
+                check_ref();
+            }
+
+            check_ref();
+        }
+
+        Py_DECREF(two);
         Py_DECREF(one);
     }
 }
