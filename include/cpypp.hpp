0bd1f760b19aa98ec986c0f05ee4085e50b76bef
Tschijnmo TSCHAU <tschijnmotschau@gmail.com>
2017-12-01 14:08:50 -0600

    Add utility to build object handles
    Currently the general utility and the building of integer values from a
    few C++ built-in types are implemented.

--- include/cpypp.hpp
+++ include/cpypp.hpp
@@ -239,6 +239,31 @@ private:
 
     bool if_borrow_;
 };
+
+//
+// Utilities for handle building
+//
+
+/** Makes a handle from the Py_BuildValue function.
+ *
+ * All the arguments are exactly forwarded to the CPython Py_BuildValue
+ * function, with the resulted objected given to the handle.
+ */
+
+template <typename... Args> Handle build_handle(Args&&... args)
+{
+    return Handle(Py_BuildValue(std::forward<Args>(args)...));
+}
+
+/** Builds a built-in Python int object.
+ */
+
+inline Handle build_int(long v) { return Handle(PyLong_FromLong(v)); }
+
+inline Handle build_int(unsigned long v)
+{
+    return Handle(PyLong_FromUnsignedLong(v));
+}
 }
 
 #endif
