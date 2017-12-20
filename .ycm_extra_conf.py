f3b2f1922f14284686e7c827ad8cb5dcd210acd2
Tschijnmo TSCHAU <tschijnmotschau@gmail.com>
2017-12-20 15:48:00 -0600

    Update YCM config for project include directory

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -1,7 +1,8 @@
+import os.path
 import subprocess
 
 def FlagsForFile(filename, **kwargs):
-    flags = ['-std=c++14', '-I/usr/local/include']
+    flags = ['-std=c++14', '-I/usr/local/include', '-I.']
 
     # Call the config script to get the directories for the Python3 interpreter
     # in PATH.
@@ -9,5 +10,9 @@ def FlagsForFile(filename, **kwargs):
         ['python3-config', '--includes']
     ).decode('utf-8').split()
     flags.extend(py_includes)
+
+    proj_dir = os.path.dirname(os.path.realpath(__file__))
+    proj_include = os.path.join(proj_dir, 'include')
+    flags.append('-I' + proj_include)
 
     return {'flags': flags}
